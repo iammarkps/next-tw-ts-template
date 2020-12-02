6a02d38504fe1d448dd7eb1a2ef9f057ff788719
Possawat Suksai <markkung08@gmail.com>
2020-12-02 16:34:16 +0700

    Init Project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => (
+  <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="text-6xl font-bold font-display">
+      Next.JS + Tailwind CSS + TypeScript
+    </div>
+  </div>
+)
+
+export default Index
